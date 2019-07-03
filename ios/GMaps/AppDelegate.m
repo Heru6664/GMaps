1b878f9c3ceaf442ecc1730b282a314ab1d6f533
Heru Julyanto <winter@Herus-MacBook-Pro.local>
2019-07-03 12:37:57 +0700

    configure maps for ios

--- ios/GMaps/AppDelegate.m
+++ ios/GMaps/AppDelegate.m
@@ -11,10 +11,13 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyADzFyXrtD-Mo8-YAMdjT6UWP7rZV13SLo"]; 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"GMaps"
