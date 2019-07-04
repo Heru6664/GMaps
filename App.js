3008af73a0c9cb176b31071a32662660b5e0fc86
Heru Julyanto <winter@Herus-MacBook-Pro.local>
2019-07-04 14:16:12 +0700

    show longlat

--- App.js
+++ App.js
@@ -1,31 +1,109 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import MapView, { PROVIDER_GOOGLE, MAP_TYPES } from "react-native-maps";
+
+const { width, height } = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+    };
+  }
+
+  jumpRandom() {
+    this.setState({ region: this.randomRegion() });
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  animateRandomCoordinate() {
+    this.map.animateCamera({ center: this.randomCoordinate() });
+  }
+
+  randomCoordinate() {
+    const region = this.state.region;
+    return {
+      latitude:
+        region.latitude + (Math.random() - 0.5) * (region.latitudeDelta / 2),
+      longitude:
+        region.longitude + (Math.random() - 0.5) * (region.longitudeDelta / 2)
+    };
+  }
+
+  randomRegion() {
+    return {
+      ...this.state.region,
+      ...this.randomCoordinate()
+    };
+  }
+
   render() {
     return (
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: 13.139238380834923,
-          longitude: 80.25188422300266,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-        showsUserLocation={true}
-      />
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          ref={ref => {
+            this.map = ref;
+          }}
+          mapType={MAP_TYPES.TERRAIN}
+          style={styles.map}
+          initialRegion={this.state.region}
+          onRegionChange={region => this.onRegionChange(region)}
+          showsUserLocation={true}
+        />
+        <View style={[styles.bubble, styles.latlng]}>
+          <Text style={styles.centeredText}>
+            {this.state.region.latitude.toPrecision(7)}
+            {",  "}
+            {this.state.region.longitude.toPrecision(7)}
+          </Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.jumpRandom()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text style={styles.buttonText}>Jump</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => this.animateRandomCoordinate()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text style={styles.buttonText}>Animate </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center"
   },
   welcome: {
     fontSize: 20,
@@ -33,7 +111,29 @@ const styles = StyleSheet.create({
     margin: 10
   },
   map: {
-    height: 100,
-    flex: 1
+    ...StyleSheet.absoluteFillObject
+  },
+  bubble: {
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20
+  },
+  latlng: {
+    width: 200,
+    alignItems: "stretch"
+  },
+  centeredText: { textAlign: "center" },
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 20,
+    backgroundColor: "transparent"
+  },
+  button: {
+    width: 100,
+    paddingHorizontal: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 5
   }
 });
