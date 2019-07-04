1918315fe3e43297bb4e026730de3fee44823307
Heru Julyanto <winter@Herus-MacBook-Pro.local>
2019-07-04 14:36:00 +0700

    marker+camera animate to coordinate

--- App.js
+++ App.js
@@ -4,15 +4,21 @@ import {
   Text,
   View,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
+  Platform
 } from "react-native";
-import MapView, { PROVIDER_GOOGLE, MAP_TYPES } from "react-native-maps";
+import MapView, {
+  PROVIDER_GOOGLE,
+  MAP_TYPES,
+  Marker,
+  AnimatedRegion
+} from "react-native-maps";
 
 const { width, height } = Dimensions.get("window");
 
 const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
+const LATITUDE = -6.192751;
+const LONGITUDE = 106.8344;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
@@ -25,12 +31,12 @@ export default class App extends Component {
         longitude: LONGITUDE,
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA
+      },
+      coordinate: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE
       }
     };
-  }
-
-  jumpRandom() {
-    this.setState({ region: this.randomRegion() });
   }
 
   onRegionChange(region) {
@@ -38,7 +44,20 @@ export default class App extends Component {
   }
 
   animateRandomCoordinate() {
-    this.map.animateCamera({ center: this.randomCoordinate() });
+    const { coordinate } = this.state;
+    const newCoordinate = {
+      latitude: LATITUDE + (Math.random() - 0.5) * (LATITUDE_DELTA / 2),
+      longitude: LONGITUDE + (Math.random() - 0.5) * (LONGITUDE_DELTA / 2)
+    };
+
+    if (Platform.OS === "android") {
+      if (this.marker) {
+        this.marker._component.animateMarkerToCoordinate(newCoordinate, 500);
+        this.map.animateCamera({ center: newCoordinate });
+      }
+    } else {
+      coordinate.timing(newCoordinate).start();
+    }
   }
 
   randomCoordinate() {
@@ -48,13 +67,6 @@ export default class App extends Component {
         region.latitude + (Math.random() - 0.5) * (region.latitudeDelta / 2),
       longitude:
         region.longitude + (Math.random() - 0.5) * (region.longitudeDelta / 2)
-    };
-  }
-
-  randomRegion() {
-    return {
-      ...this.state.region,
-      ...this.randomCoordinate()
     };
   }
 
@@ -71,7 +83,14 @@ export default class App extends Component {
           initialRegion={this.state.region}
           onRegionChange={region => this.onRegionChange(region)}
           showsUserLocation={true}
-        />
+        >
+          <Marker.Animated
+            ref={marker => {
+              this.marker = marker;
+            }}
+            coordinate={this.state.coordinate}
+          />
+        </MapView>
         <View style={[styles.bubble, styles.latlng]}>
           <Text style={styles.centeredText}>
             {this.state.region.latitude.toPrecision(7)}
@@ -80,13 +99,6 @@ export default class App extends Component {
           </Text>
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            onPress={() => this.jumpRandom()}
-            style={[styles.bubble, styles.button]}
-          >
-            <Text style={styles.buttonText}>Jump</Text>
-          </TouchableOpacity>
-
           <TouchableOpacity
             onPress={() => this.animateRandomCoordinate()}
             style={[styles.bubble, styles.button]}
